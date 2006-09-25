fa6144b88261b44bdd3f5b095638444da521fe6f
amcnabb <amcnabb@d45feaca-28bd-0310-b92a-e05ea9ac156b>
2006-09-25 23:18:36 +0000

    more pso
    git-svn-id: https://aml.cs.byu.edu/svn/python/aml/opt@4089 d45feaca-28bd-0310-b92a-e05ea9ac156b

---
+++ cubes/cube.py
@@ -0,0 +1,358 @@
+"""
+cube.py
+
+Defines a hypercube class, which should be subclassed.  The idea is that
+structures that deal with various types of hypercubic interpolation (there
+are many!) can just assume access to a class of this kind.
+"""
+
+#---------------------------------------------------------------------------
+
+from __future__ import division
+from itertools import izip
+from math import floor, ceil
+import sys
+import copy
+if sys.version < '2.3': raise RuntimeError("Python 2.3 or higher required")
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
+class Cube(object):
+    def __init__( self, constraints, values=None ):
+        """Creates a new cube object.  Requires constraints at a minimum,
+        but also allows values (of unspecified format) to be passed in."""
+        self.constraints = copy.deepcopy( constraints )
+        self.center = Vertex( [(cl + cr) / 2 for cl, cr in constraints] )
+        self.lengths = [abs(cr - cl) for cl, cr in constraints]
+        self.dims = len(constraints)
+        self.vertices = self._init_vertices( values )
+
+    def _init_vertices( self, values ):
+        """This must be overridden so as to initialize and return the
+        vertices of the cube.
+        """
+        return ()
+
+    def itervertices( self ):
+        """This must be overridden to return something akin to a vertex.
+        If the cube has no concept of vertices (a regular CMAC, for
+        example), then it may be viewed as having a single vertex in the
+        center.
+        """
+        return iter(self.vertices)
+
+    def itervertweights( self, vec ):
+        """Returns an interator over pairs: (vertex index, weight) for
+        interpolation.
+        """
+        raise NotImplementedError( "Cube.itervertweights" )
+
+    def random_vec( self, robj ):
+        """Return a random vector within the constraints of this cube.
+        """
+        return [robj.uniform(*c) for c in self.constraints]
+
+    def constrained_vec( self, vec, use_abs=False ):
+        """Returns a new vector that is constrained to be in bounds.
+        """
+        vec = copy.deepcopy(vec)
+        self.constrain_vec( vec, use_abs )
+        return vec
+
+    def constrain_vec( self, vec, use_abs = False ):
+        """Changes a vector to be within constraints"""
+        for i, v in enumerate(vec):
+            l, r = self.constraints[i]
+            if use_abs:
+                length = abs(l-r)
+                vsize = abs(v)
+                if vsize > length:
+                    vec[i] = v * length/vsize
+            else:
+                if v < l: vec[i] = l
+                if v > r: vec[i] = r
+
+    def wrap( self, vec ):
+        for i, (v, (l,r)) in enumerate(izip(vec,self.constraints)):
+            span = r - l
+            if v > r:
+                extra = v - r
+                wrap = extra - (extra // span) * span
+                vec[i] = l + wrap
+            elif v < l:
+                extra = v - l
+                # With the minus sign outside, the floor would become a ceil,
+                # so we leave it inside.
+                wrap = extra + (-extra // span) * span
+                vec[i] = r + wrap
+
+    def wrapped_vec( self, vec ):
+        """Wraps a vector so that it appears to be on a torus.
+        """
+        vec = copy.deepcopy(vec)
+        self.wrap( vec )
+        return vec
+
+    def iterspace( self, resolution=None ):
+        if resolution is None:
+            resolution = 100
+        if isinstance(resolution, (int, long)):
+            resolution = (resolution,) * self.dims
+
+        # The idea here is that we iterate over the space at the given
+        # resolution, passing out exactly resolution[d] points for dimension d.
+
+        v = [0] * self.dims
+        vec = [cl for cl, cr in self.constraints]
+        yield vec
+        while True:
+            for d in xrange(self.dims):
+                v[d] += 1
+                cl, cr = self.constraints[d]
+                if v[d] < resolution[d]:
+                    vec[d] = (v[d] / (resolution[d]-1)) * (cr - cl) + cl
+                    yield vec
+                    break
+                else:
+                    vec[d] = cl
+                    v[d] = 0
+            else:
+                # We turned over.  We're through
+                break
+
+    def is_inside( self, vec ):
+        for i, (cl, cr) in enumerate(self.constraints):
+            if (vec[i] is not None) and (not (cl <= vec[i] <= cr)):
+                return False
+        return True
+
+    def train_err( self, vec, valchange ):
+        """Trains values based on a value error at the given location."""
+        for vidx, w in self.itervertweights( vec ):
+            self.vertices[vidx].value += valchange * w
+
+    def value( self, vec ):
+        """Returns the value at the location of the given vector."""
+        val = 0.0
+        for vidx, w in self.itervertweights(vec):
+            val += self.vertices[vidx].value * w
+        return val
+
+    def __call__( self, vec ):
+        # I can't just set this function here.  It would always call the
+        # wrong one.
+        return self.value( vec )
+
+    def split( self, dim, distance=0.5 ):
+        """Returns left and right children of this cube, given the dimension
+        in which to split and the distance along that dimension (defaults to
+        0.5, or halfway).
+        """
+        raise NotImplementedError( "Cube.split" )
+
+    def __str__( self ):
+        s = ""
+        for v in self.itervertices():
+            s = s + ("%r:%r\n" % (v,v.value))
+        return s
+
+#---------------------------------------------------------------------------
+
+class ConstantGradientExtremumMixin(object):
+    def constrained_extremum( self, cvec, ismin=False ):
+        con = self.constraints
+        g = self.gradient()
+        pt = Vertex(cvec)
+
+        # The index of the constraint to use for max vs. min:
+        # If not minimum (max), then we use the leftmost value for negative
+        # gradient elements.  Otherwise, we use the rightmost.
+        lesseridx = int(ismin)
+        greateridx = int(not ismin)
+
+        for i, c in enumerate(cvec):
+            # If this is an unconstrained dimension, then we follow the
+            # gradient in this dimension.
+            # Otherwise, we leave this coordinate alone (it's constrained)
+            if c is None:
+                if g[i] < 0:
+                    pt[i] = con[i][lesseridx]
+                else:
+                    pt[i] = con[i][greateridx]
+        pt.value = self.value( pt )
+        return pt
+
+#---------------------------------------------------------------------------
+
+class Vertex(list):
+    # Limit the things we can change here
+    __slots__ = ['value','cube']
+
+    def train_val( self, newval ):
+        """These allow us to defer to our containers when it makes sense to
+        do so.
+        """
+        if hasattr( self, 'cube' ):
+            self.cube.train_val( self, newval )
+        else:
+            self.value = newval
+
+    def train_err( self, error ):
+        """These allow us to defer to our containers when it makes sense to
+        do so.
+        """
+        if hasattr( self, 'cube' ) and self.cube is not None:
+            self.cube.train_err( self, error )
+        else:
+            self.value += error
+
+    def __lt__( self, other ):
+        return self.value < other.value
+
+    def __eq__( self, other ):
+        return self.value == other.value
+
+    def __gt__( self, other ):
+        return self.value > other.value
+
+    def __le__( self, other ):
+        return self.value <= other.value
+
+    def __ge__( self, other ):
+        return self.value >= other.value
+
+    def __init__( self, *args, **kargs ):
+        # Call the original constructor (try not to mess list up too much)
+        val = None
+        cube = None
+        if kargs.has_key('value'):
+            val = kargs['value']
+            del kargs['value']
+        if kargs.has_key('cube'):
+            cube = kargs['cube']
+            del kargs['cube']
+
+        super( Vertex, self ).__init__(*args, **kargs)
+
+        # Add our own stuff to this
+        if val is not None:
+            self.value = val
+        elif len(args) > 0 and isinstance(args[0], Vertex):
+            self.value = args[0].value
+        else:
+            self.value = 0
+
+        if cube is not None:
+            self.cube = cube
+        elif len(args) > 0 and isinstance(args[0], Vertex):
+            self.cube = args[0].cube
+        else:
+            self.cube = None
+
+    def __add__( self, other ):
+        """Add two vertices
+        
+        other: another list (same length) or something to be added to each
+            element
+        """
+
+        newvert = Vertex( self )
+        if isinstance(other, (list, tuple)):
+            for i, x in enumerate(newvert):
+                newvert[i] += other[i]
+        else:
+            for i, x in enumerate(newvert):
+                newvert[i] += other
+
+        if isinstance(other, Vertex):
+            newvert.value += other.value
+
+        return newvert
+
+    __radd__ = __add__
+
+    def __iadd__( self, other ):
+        new = self.__add__( other )
+        self.value = new.value
+        for i,v in enumerate(new):
+            self[i] = v
+        return self
+
+    def __sub__( self, other ):
+        """Subtract two vertices
+        
+        other: another list (same length) or something to be subtracted from
+            each element
+        """
+
+        newvert = Vertex( self )
+        if isinstance(other, (list, tuple)):
+            for i, x in enumerate(newvert):
+                newvert[i] -= other[i]
+        else:
+            for i, x in enumerate(newvert):
+                newvert[i] -= other
+
+        if isinstance(other, Vertex):
+            newvert.value -= other.value
+
+        return newvert
+
+    def __isub__( self, other ):
+        new = self.__sub__( other )
+        self.value = new.value
+        for i,v in enumerate(new):
+            self[i] = v
+        return self
+
+    def __rsub__( self, other ):
+        """Subtract two vertices
+        
+        other: another list (same length) or something to subtract each element
+            from
+        """
+
+        newvert = Vertex( self )
+        if isinstance(other, (list, tuple)):
+            for i, x in enumerate(newvert):
+                newvert[i] = other[i] - x
+        else:
+            for i, x in enumerate(newvert):
+                newvert[i] = other - x
+
+        if isinstance(other, Vertex):
+            newvert.value = other.value - newvert.value
+
+        return newvert
+
+    def __neg__( self ):
+        newvert = Vertex( self )
+        for i, x in enumerate( newvert ):
+            newvert[i] = -x
+
+        return newvert
+
+    def __div__( self, val ):
+        newvert = copy.deepcopy(self)
+        if isinstance(val, (int, long, float)):
+            for i, x in enumerate(self):
+                newvert[i] = x / val
+        else:
+            raise RuntimeError, "Can't divide a vertex by anything but a scalar"
+        return newvert
+
+    __truediv__ = __div__
+
+    def __mul__( self, val ):
+        newvert = copy.deepcopy(self)
+        if isinstance(val, (int, long, float)):
+            for i, x in enumerate(self):
+                newvert[i] = x * val
+        else:
+            raise RuntimeError, "Can't multiply a vertex by anything but a scalar"
+        return newvert
+
+    __rmul__ = __mul__
+
+#---------------------------------------------------------------------------
---
+++ cubes/cube_kuhn.py
@@ -0,0 +1,169 @@
+"""
+cube_kuhn.py
+
+Defines a hypercube that does Kuhn triangulation for interpolation
+between its vertices.
+"""
+
+from __future__ import division
+
+from cube import Cube, Vertex, ConstantGradientExtremumMixin
+from itertools import izip
+from copy import deepcopy
+
+class CubeKuhn(Cube):
+    def __init__( self, *args, **kargs ):
+        super( CubeKuhn, self ).__init__( *args, **kargs )
+        self.origin = Vertex([cl for cl, cr in self.constraints], cube=self)
+
+    def iterdefaultvals( self ):
+        for x in xrange(2**self.dims):
+            yield 0
+
+    def _init_vertices( self, values ):
+        """Initialize the vertices of this cube.  The vertices are located
+        at the corners.  We expect to get all 2**d of them in binary
+        decomposition order. (least significant bit is in the 0 position
+        of the array, which seems backwards, but is correct)
+        """
+        if values is None:
+            values = self.iterdefaultvals()
+        vertices = []
+        cons = self.constraints
+        for vi, val in enumerate( values ):
+            m = self.dims - 1
+            # To switch the order of decomposition, just replace the
+            # 'x' with 'self.dims - 1 - x'
+            # Currently the most significant bit is in the zeroth
+            # element, which works well for later sorting of stuff.
+            vtx = [c[ (vi >> x) & 1 ] for x, c in enumerate(cons)]
+            vtx = Vertex( vtx, value=val, cube=self )
+            vertices.append( vtx )
+        return vertices
+
+    def itervertexweights( self, vec ):
+        #ki = self.kuhn_info( vec )
+        #return iter([(self.vertices[i],w) for i, w in izip(*ki)])
+        verts = list(self.itervertices())
+        weights = [0] * len(verts)
+        for i, v in enumerate(verts):
+            d = reduce(lambda x,y: x+(y[0]-y[1])**2, izip(vec, v),0)
+            if d == 0.0:
+                weights = [0] * len(verts)
+                weights[i] = 1
+                break
+            weights[i] = 1/d
+        s = sum(weights)
+        weights = [w/s for w in weights]
+        return izip(verts,weights)
+        #ki = self.kuhn_info( vec )
+        #indices, coords = ki
+        #verts = [self.vertices[i] for i in indices]
+        #weights = [0] * len(verts)
+        #for i, v in enumerate(verts):
+        #    d = reduce(lambda x,y: x+(y[0]-y[1])**2, izip(vec, v),0)
+        #    if d == 0.0:
+        #        weights = [0] * len(verts)
+        #        weights[i] = 1
+        #        break
+        #    weights[i] = 1/d
+        #s = sum(weights)
+        #weights = [w/s for w in weights]
+        #return izip(verts,weights)
+
+    def value( self, vec ):
+        """Get the value at this location."""
+        val = 0
+        for v, w in self.itervertexweights( vec ):
+            val += v.value * w
+
+        return val
+
+    def split( self, dim, distance=0.5 ):
+        """Split this cube into left and right children, given the dimension
+        in which to split and a distance along that dimension.  Distance
+        defaults to 0.5 (halfway).
+        """
+        # We have to create a bunch of new vertices around the midpoint of
+        # things.  The two new cubes created will have their own copy of things
+        # just to make everything simpler.
+        cl, cr = self.constraints[dim]
+        mid = cr * distance + cl * (1-distance)
+
+        leftconstraints = list( self.constraints )
+        rightconstraints = list( self.constraints )
+
+        leftconstraints[dim] = cl, mid
+        rightconstraints[dim] = mid, cr
+
+        leftvalues = [deepcopy(x.value) for x in self.vertices]
+        rightvalues = deepcopy(leftvalues)
+
+        # Now, we iterate over all numbers 0 ... 2**d-1.  If something is on
+        # the "left" of the appropriate dimension, we change the vertex of the
+        # right cube.  If something is on the "right", we change the vertex of
+        # the left.
+        dmask = 1 << dim
+        for vidx, vert in enumerate(self.vertices):
+            # We always change one of left and right, never both.
+            if vidx & dmask:
+                # This index represents a vertex to the "right" of the divide,
+                # so we change it in the "left" list to be the middle value,
+                # thus chopping it off at the split.
+                lv = Vertex(vert)
+                lv[dim] = mid
+                # We also need to set its value, so we set it to the original
+                # interpolated value.
+                leftvalues[vidx] = self.value( lv )
+            else:
+                # On the left, so chop off the right vertex coordinate.
+                rv = Vertex(vert)
+                rv[dim] = mid
+                rightvalues[vidx] = self.value( rv )
+
+        lchild = CubeKuhn( leftconstraints, leftvalues )
+        rchild = CubeKuhn( rightconstraints, rightvalues )
+
+        return lchild, rchild
+
+    #-----------------------------------------------------------------------
+    # Not inherited stuff
+    #-----------------------------------------------------------------------
+    def kuhn_info( self, vec ):
+        """Returns the vertex indices in question and the barycentric
+        coordinates of this point."""
+        dims = self.dims
+        dsize = self.lengths
+
+        # Get this vector into the space defined by this cube (subtract
+        # the origin)
+        relvec = vec - self.origin
+
+        # We need the index so that we know where stuff got sorted to
+        # We also need everything to be normalized to some value between
+        # 0 and 1.
+        relvec = [(v/dsize[i],i) for i, v in enumerate(relvec)]
+
+        # The triangle is found by moving in the largest direction
+        # first, then the next largest, etc.
+        relvec.sort()
+        relvec.reverse()
+
+        # Now find the triangle vertex indices
+        indices = [0] # The corner is always involved
+        vertex_idx = 0 # Start at the origin
+        bary_coords = []
+        bary_val = 1.0
+        for x, idx in relvec:
+            # Set the bit corresponding to this dimension to '1'.  This
+            # constitutes walking in one dimension.
+            vertex_idx |= 0x1 << idx
+            indices.append( vertex_idx )
+            bary_coords.append( bary_val - x )
+            bary_val = x
+
+        bary_coords.append( relvec[-1][0] )
+        
+        return indices, bary_coords
+
+#---------------------------------------------------------------------------
