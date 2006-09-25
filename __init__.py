8ab3c54971ea29df397c18fb76c971b4eca1cd1a
amcnabb <amcnabb@d45feaca-28bd-0310-b92a-e05ea9ac156b>
2006-09-25 23:27:42 +0000

    Added a docstring.
    git-svn-id: https://aml.cs.byu.edu/svn/python/aml/opt@4093 d45feaca-28bd-0310-b92a-e05ea9ac156b

--- __init__.py
+++ __init__.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python -tt
 
+"""AML Optimization Library
+
+Almost all of this stuff was written by Chris Monson.  It wasn't intended
+to be put together in this exact format, so don't blame him for the
+jumbled nature of this library.
+"""
 
 # vim: et sw=4 sts=4
 
