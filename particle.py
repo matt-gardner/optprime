1f310d3201b50e52b7bfd8749d0445b5be86234f
amcnabb <amcnabb@d45feaca-28bd-0310-b92a-e05ea9ac156b>
2006-12-05 22:55:16 +0000

    Particles now have a __repr__, and you can initialize them from their repr. You can also do gbest in the particle.
    git-svn-id: https://aml.cs.byu.edu/svn/python/aml/opt@4274 d45feaca-28bd-0310-b92a-e05ea9ac156b

--- particle.py
+++ particle.py
@@ -4,22 +4,76 @@ from Vector import Vector
 import operator
 
 #------------------------------------------------------------------------------
-class Particle(Vector):
-    def __init__( self, pos, vel=None, val=None ):
-        super(Particle, self).__init__( pos )
+class Particle(object):
+    """Particle for Particle Swarm Optimization.
+    
+    Initialization can be done by either a repr string or a position (with
+    optional velocity, value, and particle id).
+
+    Sample repr:
+    14  0,1,2,3,4;4.3,-2.1;-0.4,0.7;0.0074;4.3,-2.1;0.0074;1.8,0.5;0.00023
+    Interpretation of repr:
+    id     deps     pos       vel    val     pbpos   pbval  gbpos   gbval
+
+    Note: A particle's global best is only used in the MapReduce
+    implementation--the neighborhood stuff for the normal PSO implementation
+    needs to be converted over somehow.
+
+    Also note: the old implementation had Particle inheriting from Vector,
+    with the position and the particle being the same thing.  This is
+    awkward, so we stopped doing that.  Hopefully other code won't depend
+    on it too much.
+    """
+    def __init__( self, param, vel=None, val=None, pid=None ):
+        repr_state = None
+        try:
+            repr_id, repr_state = param.split('\t')
+            pid = int(repr_id)
+        except AttributeError:
+            pos = param
+
+        if repr_state is not None:
+            dep_str, pos_str, vel_str, val_str, pbestpos_str, pbestval_str, \
+                    gbestpos_str, gbestval_str = repr_state.split(';')
+            deps = [int(field) for field in dep_str.split(',')]
+            pos = [float(field) for field in pos_str.split(',')]
+            vel = [float(field) for field in vel_str.split(',')]
+            val = float(val_str)
+            bestpos = [float(field) for field in pbestpos_str.split(',')]
+            bestval = float(pbestval_str)
+            gbestpos = [float(field) for field in gbestpos_str.split(',')]
+            gbestval = float(gbestval_str)
+
+        #super(Particle, self).__init__( pos )
+
+        if pid is not None:
+            self.id = pid
+        else:
+            self.id = self.next_id
+            self.next_id += 1
 
         self.dims = len(pos)
-        self.pos = self
 
         if vel is None:
             vel = [0.0] * self.dims
         if val is None:
-            val = 0.0
+            val = float('inf')
 
+        if repr_state is None:
+            bestpos = pos
+            bestval = val
+            gbestpos = [0.0] * self.dims
+            gbestval = val
+            deps = [self.id]
+
+        self.deps = deps
+        self.pos = Vector(pos)
         self.vel = Vector(vel)
         self.val = val
-        self.bestpos = Vector(self.pos)
-        self.bestval = self.val
+        self.bestpos = Vector(bestpos)
+        self.bestval = bestval
+        self.gbestpos = Vector(gbestpos)
+        self.gbestval = gbestval
 
         self.stagnantcount = 0
         self.improvedcount = 0
@@ -28,6 +82,8 @@ class Particle(Vector):
         p = Particle(self.pos, self.vel, self.val)
         p.bestpos = self.bestpos
         p.bestval = self.bestval
+        p.gbestpos = self.gbestpos
+        p.gbestval = self.gbestval
         return p
 
     def update( self, newpos, newvel, newval, isbetterfunc=operator.lt ):
@@ -63,5 +119,19 @@ class Particle(Vector):
     def __str__( self ):
         return "pos: %r; vel: %r; val: %r; bestpos: %r; bestval: %r" % (
                 self.pos, self.vel, self.val, self.bestpos, self.bestval)
+
+    def __repr__( self ):
+        return '\t'.join((str(self.id),
+                ';'.join((','.join(str(dep) for dep in self.deps),
+                        ','.join(str(x) for x in self.pos),
+                        ','.join(str(x) for x in self.vel),
+                        str(self.val),
+                        ','.join(str(x) for x in self.bestpos),
+                        str(self.bestval),
+                        ','.join(str(x) for x in self.gbestpos),
+                        str(self.gbestval)))))
+
+    next_id = 0
+
 
 #------------------------------------------------------------------------------
