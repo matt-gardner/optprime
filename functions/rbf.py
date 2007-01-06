120c63520e3e2cb9855bea1776c16a2844778ea2
amcnabb <amcnabb@d45feaca-28bd-0310-b92a-e05ea9ac156b>
2007-01-06 00:02:55 +0000

    More little RBF changes, esp. with data generation.
    git-svn-id: https://aml.cs.byu.edu/svn/python/aml/opt@4302 d45feaca-28bd-0310-b92a-e05ea9ac156b

--- functions/rbf.py
+++ functions/rbf.py
@@ -1,8 +1,10 @@
 from __future__ import division
-from itertools import izip
+from itertools import izip, chain
 from scipy import stats
 import _general
 from iterextra import itergroup
+
+RBF_STDDEV = 10
 
 class RBF(_general._Base):
     """Radial Basis Function Network
@@ -45,17 +47,62 @@ class RBF(_general._Base):
 
     def rbf_value( self, params, point ):
         """Return the value of a single basis function at a given point."""
-        output_weight = params[0]
+        output_weight = abs(params[0])
         param_iter = itergroup(params[1:], 2)
 
-        sq_dist = sum((weight * (x - center) ** 2) for (weight, center), x in \
-                        izip(param_iter, point))
+        sq_dist = sum((abs(weight) * (x - center) ** 2) for
+                (weight, center), x in izip(param_iter, point))
 
-        return output_weight * stats.norm.pdf(sq_dist ** 0.5)
+        return output_weight * stats.norm.pdf(sq_dist ** 0.5, scale=RBF_STDDEV)
 
 
 if __name__ == '__main__':
-    rbf = RBF(dims=3, datafile='rbftest.csv')
+    # Generate data, picking a random RBF
+    import optparse
+    parser = optparse.OptionParser()
+    parser.add_option('-b', '--bases', dest='nbases', type='int',
+            help='Number of Basis Functions')
+    parser.add_option('-p', '--points', dest='npoints', type='int',
+            help='Number of Points')
+    parser.add_option('-d', '--inputdims', dest='inputdims', type='int',
+            help='Number of Input Dimensions')
+    parser.add_option('-r', '--randomseed', dest='rand', type='int',
+            help='Random Seed for Bases')
+    parser.set_defaults(nbases=2, npoints=1000, inputdims=1, rand=0)
+
+    options, args = parser.parse_args()
+    nbases = options.nbases
+    inputdims = options.inputdims
+    npoints = options.npoints
+    import random
+    rand = random.Random(options.rand)
+
+    dims = nbases * (1 + 2 * inputdims)
+    rbf = RBF(dims=dims)
+    rbf.inputdims = inputdims
+
+    # We'll limit constraints a bit for our generated data.
+    center_constraints = []
+    for i in xrange(nbases):
+        center_constraints.append((0, 50))
+        for j in xrange(inputdims):
+            center_constraints.append((0, 50))
+            center_constraints.append((-50, 50))
+            # Use this if you want to make the RBF centers more evenly spaced:
+            #center_constraints.append((-50 + 100 * i / nbases,
+            #    -50 + 100 * (i + 1) / nbases))
+    from aml.opt.cubes.cube import Cube
+    vec = Cube(center_constraints).random_vec(rand)
+    from sys import stderr
+    print >>stderr, 'Vec:', ','.join(str(x) for x in vec)
+
+    inputs_constraints = ((-50, 50),) * inputdims
+    inputs_cube = Cube(inputs_constraints)
+    for i in xrange(npoints):
+        point = inputs_cube.random_vec(rand)
+        print ','.join(str(x) for x in chain(point, (rbf.net_value(vec, point),)))
+
+    #rbf = RBF(dims=3, datafile='rbftest.csv')
     #rbf.inputdims = 1
-    #rbf.net_value((1.0, 1.0, 0.0), 0.0)
-    print rbf((1.0, 1.0, 0.0))
+    #print rbf.net_value((1.0, 1.0, 0.0), 0.0)
+    #print rbf((1.0, 1.0, 0.0))
