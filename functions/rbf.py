5b2d612ff266e92ba57cb58702239a6981d51f5b
amcnabb <amcnabb@d45feaca-28bd-0310-b92a-e05ea9ac156b>
2007-01-12 01:50:18 +0000

    changed how we create tempfiles for rbf
    git-svn-id: https://aml.cs.byu.edu/svn/python/aml/opt@4351 d45feaca-28bd-0310-b92a-e05ea9ac156b

--- functions/rbf.py
+++ functions/rbf.py
@@ -17,26 +17,32 @@ class RBF(_general._Base):
     """Radial Basis Function Network
 
     The function takes an RBF and returns the sum squared error over the
-    dataset in 'datafile'.
+    dataset in the first file in 'datafiles'.
 
     The 'dims' parameter should be num_bases * (1 + 2 * num_input_dimensions)
-    where num_input_dimensions is defined by the data found in 'datafile'.
+    where num_input_dimensions is defined by the data found in the datafile.
     The portion of 'vec' for each basis ends up looking like:
     (output_weight, weight1, center1, weight2, center2, ...)
 
     Note: we do not yet set the deviation/variance for each basis function.
     """
-    _args = [('datafile', 'rbfdata.csv', \
-                    'File with training data (CSV format)')]
+    _args = [('datafiles', '', 'File with training data (CSV format)'),
+            ('points', '1000', 'Number of generated data points'),
+            ('inputdims', '1', 'Dimensionality of generated points'),
+            ('seed', '42', 'Random seed used to generate points')]
     def __init__( self, *args, **kargs):
         super(RBF,self).__init__( *args, **kargs )
         self._set_constraints( ((-50,50),) * self.dims )
         self.inputdims = None
+        try:
+            datafile = self.datafiles.split()[0]
+            self.datapoints = [tuple(float(field) for field in line.split(','))
+                            for line in open(datafile)]
+        except (IndexError, IOError):
+            self.datapoints = []
 
     def __call__( self, vec ):
         """Evaluate sum squared error."""
-        datapoints = (tuple(float(field) for field in line.split(','))
-                        for line in open(self.datafile))
         sumsqerr = 0.0
         for point in datapoints:
             # Figure out the dimensionality of our dataset by looking at the
@@ -62,36 +68,56 @@ class RBF(_general._Base):
 
         return output_weight * gaussian(sq_dist ** 0.5, scale=RBF_STDDEV)
 
+    def tmpfiles(self):
+        """Generate some points in a temporary file and return the filename."""
+        import random
+        rand = random.Random(self.seed)
+
+        nbases = int(self.dims / (1 + 2 * self.inputdims))
+
+        # We'll limit constraints a bit for our generated data.
+        center_constraints = []
+        for i in xrange(nbases):
+            center_constraints.append((0, 50))
+            for j in xrange(inputdims):
+                center_constraints.append((0, 50))
+                center_constraints.append((-50, 50))
+                # Use this if you want to make the RBF centers more evenly spaced:
+                #center_constraints.append((-50 + 100 * i / nbases,
+                #    -50 + 100 * (i + 1) / nbases))
+        from aml.opt.cubes.cube import Cube
+        vec = Cube(center_constraints).random_vec(rand)
+        from sys import stderr
+        print >>stderr, 'Vector used to generate points:', \
+                ','.join(str(x) for x in vec)
+
+        import os, tempfile
+        csvfd, csvfilename = tempfile.mkstemp()
+        csvfile = os.fdopen(csvfd, 'w')
+        inputs_constraints = ((-50, 50),) * inputdims
+        inputs_cube = Cube(inputs_constraints)
+        for i in xrange(npoints):
+            point = inputs_cube.random_vec(rand)
+            self.datapoints.append(point)
+            print >>csvfile, ','.join(str(x) for x in
+                    chain(point, (self.net_value(vec, point),)))
+        csvfile.close()
+        return (csvfilename,)
 
 def generate_points(bases, points, inputdims, randomseed):
     """Create a random RBF and generate points with it."""
-    import random
-    rand = random.Random(options.rand)
 
     dims = nbases * (1 + 2 * inputdims)
     rbf = RBF(dims=dims)
     rbf.inputdims = inputdims
+    rbf.points = points
 
-    # We'll limit constraints a bit for our generated data.
-    center_constraints = []
-    for i in xrange(nbases):
-        center_constraints.append((0, 50))
-        for j in xrange(inputdims):
-            center_constraints.append((0, 50))
-            center_constraints.append((-50, 50))
-            # Use this if you want to make the RBF centers more evenly spaced:
-            #center_constraints.append((-50 + 100 * i / nbases,
-            #    -50 + 100 * (i + 1) / nbases))
-    from aml.opt.cubes.cube import Cube
-    vec = Cube(center_constraints).random_vec(rand)
-    from sys import stderr
-    print >>stderr, 'Vec:', ','.join(str(x) for x in vec)
-
-    inputs_constraints = ((-50, 50),) * inputdims
-    inputs_cube = Cube(inputs_constraints)
-    for i in xrange(npoints):
-        point = inputs_cube.random_vec(rand)
-        print ','.join(str(x) for x in chain(point, (rbf.net_value(vec, point),)))
+    csvfilename = rbf.tmpfiles()[0]
+    csvfile = open(csvfilename)
+    for line in csvfile:
+        print line,
+    from os import unlink
+    unlink(csvfilename)
 
 
 if __name__ == '__main__':
@@ -112,6 +138,8 @@ if __name__ == '__main__':
     nbases = options.nbases
     inputdims = options.inputdims
     npoints = options.npoints
+
+    generate_points(nbases, npoints, inputdims, options.rand)
 
     #rbf = RBF(dims=3, datafile='rbftest.csv')
     #rbf.inputdims = 1
