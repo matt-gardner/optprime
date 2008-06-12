79160fd13de05bfdbea091188cf8953548f4b07e
Matt Gardner <mjg82@byu.edu>
2008-06-12 10:59:20 -0600

    just a test

---
+++ functions/test.py
@@ -0,0 +1,16 @@
+import _general
+
+class Test(_general._Base):
+    def __init__(self, *args, **kargs):
+        super(Test,self).__init__( *args, **kargs)
+        self._set_constraints(((3,200), (4,100), (0,302), (0,200),)*3)
+
+    def __call__(self, vec):
+        retval = 0
+        for i in range(len(vec)):
+            if abs(vec[i] - 5) < .01 : retval -= 5
+        return retval
+        #return sum(vec)
+
+
+# vim: et sw=4 sts=4
