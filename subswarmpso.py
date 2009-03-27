f31de7c9aff7a310ce2f0a4bd285a605ab46075a
Andrew McNabb <amcnabb@mcnabbs.org>
2009-03-27 11:13:19 -0600

    factored out bypass_iteration and made SubswarmPSO inherit from StandardPSO

--- subswarmpso.py
+++ subswarmpso.py
@@ -5,13 +5,14 @@ import sys, optparse, operator
 
 import mrs
 from mrs import param
-from particle import Particle, Message, unpack
+import standardpso
+from particle import Swarm, Particle, Message, unpack
 
 
 # TODO: allow the initial set of particles to be given
 
 
-class StandardPSO(mrs.MapReduce):
+class SubswarmPSO(standardpso.StandardPSO):
     def __init__(self, opts, args):
         """Mrs Setup (run on both master and slave)"""
 
@@ -21,6 +22,7 @@ class StandardPSO(mrs.MapReduce):
         self.function = param.instantiate(opts, 'func')
         self.motion = param.instantiate(opts, 'motion')
         self.topology = param.instantiate(opts, 'top')
+        self.subtop = param.instantiate(opts, 'subtop')
 
         self.function.setup()
         self.motion.setup(self.function)
@@ -28,25 +30,6 @@ class StandardPSO(mrs.MapReduce):
 
     ##########################################################################
     # Bypass Implementation
-
-    def bypass(self):
-        """Run a "native" version of PSO without MapReduce."""
-
-        if not self.cli_startup():
-            return
-
-        # Perform the simulation in batches
-        try:
-            for batch in xrange(self.opts.batches):
-                # Separate by two blank lines and a header.
-                print
-                print
-                if (self.opts.batches > 1):
-                    print "# Batch %d" % batch
-                self.bypass_batch(batch)
-                print "# DONE" 
-        except KeyboardInterrupt, e:
-            print "# INTERRUPTED"
 
     def bypass_batch(self, batch):
         """Performs a single batch of PSO without MapReduce.
@@ -59,7 +42,8 @@ class StandardPSO(mrs.MapReduce):
 
         # Create the Population.
         rand = self.initialization_rand(batch)
-        particles = list(self.topology.newparticles(batch, rand))
+        subswarms = [Swarm(self.subtop.newparticles(batch, rand))
+                for i in xrange(self.topology.num)]
 
         # Perform PSO Iterations.  The iteration number represents the total
         # number of function evaluations that have been performed for each
@@ -82,21 +66,6 @@ class StandardPSO(mrs.MapReduce):
         output.finish()
 
         self.cleanup()
-
-    def bypass_iteration(self, particles):
-        # Update position and value.
-        for p in particles:
-            self.move_and_evaluate(p)
-
-        # Communication phase.
-        comp = self.function.comparator
-        for p in particles:
-            for i in self.topology.iterneighbors(p):
-                # Send p's information to neighbor i.
-                neighbor = particles[i]
-                neighbor.nbest_cand(p.pbestpos, p.pbestval, comp)
-                if self.opts.transitive_best:
-                    neighbor.nbest_cand(p.nbestpos, p.nbestval, comp)
 
     ##########################################################################
     # MapReduce Implementation
@@ -281,17 +250,6 @@ class StandardPSO(mrs.MapReduce):
     ##########################################################################
     # Helper Functions (shared by bypass and mrs implementations)
 
-    def move_and_evaluate(self, p):
-        """Moves, evaluates, and updates the given particle."""
-        self.set_particle_rand(p)
-        if p.iters > 0:
-            newpos, newvel = self.motion(p)
-        else:
-            newpos, newvel = p.pos, p.vel
-        # TODO(?): value = self.function(newpos, p.rand)
-        value = self.function(newpos)
-        p.update(newpos, newvel, value, self.function.comparator)
-
     def set_particle_rand(self, p):
         """Makes a Random for the given particle and saves it to `p.rand`.
 
@@ -312,132 +270,23 @@ class StandardPSO(mrs.MapReduce):
         offset = 2 + base * batch
         return self.random(offset)
 
-    def findbest(self, particles, comparator):
-        """Returns the best particle from the given list."""
-        best = None
-        bestval = None
-        for p in particles:
-            if (best is None) or comparator(p.pbestval, bestval):
-                best = p
-                bestval = p.pbestval
-        return best
-
-    def cli_startup(self):
-        """Checks whether the repository is dirty and reports options.
-
-        Returns True if startup succeeded, otherwise False.
-        """
-        import cli
-        import sys
-
-        # Check whether the repository is dirty.
-        mrs_status = cli.GitStatus(mrs)
-        amlpso_status = cli.GitStatus(sys.modules[__name__])
-        if not self.opts.hey_im_testing:
-            if amlpso_status.dirty:
-                print >>sys.stderr, (('Repository amlpso (%s) is dirty!'
-                        '  Use --hey-im-testing if necessary.') %
-                        amlpso_status.directory)
-                return False
-            if mrs_status.dirty:
-                print >>sys.stderr, (('Repository mrs (%s) is dirty!'
-                        '  Use --hey-im-testing if necessary.') %
-                        mrs_status.directory)
-                return False
-
-        # Report command-line options.
-        if not self.opts.quiet:
-            import email
-            date = email.Utils.formatdate(localtime=True)
-            print '#', sys.argv[0]
-            print '# Date:', date
-            print '# Git Status:'
-            print '#   amlpso:', amlpso_status
-            print '#   mrs:', mrs_status
-            print "# Options:"
-            for key, value in sorted(vars(self.opts).iteritems()):
-                print '#   %s = %s' % (key, value)
-            print ""
-            sys.stdout.flush()
-
-        return True
-
-    def setup(self):
-        try:
-            self.tmpfiles = self.function.tmpfiles()
-        except AttributeError:
-            self.tmpfiles = []
-
-    def cleanup(self):
-        for f in self.tmpfiles:
-            os.unlink(f)
-
 
 ##############################################################################
 # Busywork
 
 def update_parser(parser):
     """Adds PSO options to an OptionParser instance."""
-    # Set the default Mrs implementation to Bypass (instead of MapReduce).
-    parser.usage = parser.usage.replace('Serial', 'Bypass')
-    parser.set_default('mrs', 'Bypass')
-
-    parser.add_option('-q', '--quiet',
-            dest='quiet', action='store_true',
-            help='Refrain from printing version and option information',
-            default=False,
-            )
-    parser.add_option('-v','--verbose',
-            dest='verbose', action='store_true',
-            help="Print out verbose error messages",
-            default=False,
-            )
-    parser.add_option('-b','--batches',
-            dest='batches', type='int',
-            help='Number of complete experiments to run',
-            default=1,
-            )
-    parser.add_option('-i','--iters',
-            dest='iters', type='int',
-            help='Number of iterations per batch',
-            default=100,
-            )
-    parser.add_option('-f','--func', metavar='FUNCTION',
-            dest='func', action='extend', search=['functions'],
-            help='Function to optimize',
-            default='sphere.Sphere',
-            )
-    parser.add_option('-m','--motion',
-            dest='motion', action='extend', search=['motion.basic', 'motion'],
-            help='Particle motion type',
-            default='Constricted',
-            )
-    parser.add_option('-t','--top', metavar='TOPOLOGY',
-            dest='top', action='extend', search=['topology'],
-            help='Particle topology/sociometry',
+    parser = standardpso.update_parser(parser)
+    parser.add_option('-l','--link', metavar='TOPOLOGY',
+            dest='link', action='extend', search=['topology'],
+            help='Topology/sociometry for linking subswarms',
             default='Complete',
             )
-    parser.add_option('-o', '--out', metavar='OUTPUTTER',
-            dest='out', action='extend', search=['output'],
-            help='Style of output',
-            default='Basic',
+    parser.add_option('-s','--subiters',
+            dest='subiters', type='int',
+            help='Number of iterations per subswarm between iterations',
+            default=100,
             )
-    parser.add_option('-N', '--num-tasks',
-            dest='numtasks', type='int',
-            help='Number of tasks (if 0, create 1 task per particle)',
-            default=0,
-            )
-    parser.add_option('--transitive-best',
-            dest='transitive_best', action='store_true',
-            help='Whether to send nbest to others instead of pbest',
-            default=False
-            )
-    parser.add_option('--hey-im-testing',
-            dest='hey_im_testing', action='store_true',
-            help='Ignore errors from uncommitted changes (for testing only!)',
-            default=False,
-            )
-
     return parser
 
 
