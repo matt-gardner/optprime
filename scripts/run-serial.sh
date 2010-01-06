da88e20f2ea61a054ff1d98f17bca57e00e11105
Andrew McNabb <amcnabb@mcnabbs.org>
2010-01-06 13:50:57 -0700

    added two scripts I've been using for the last month or two to run things

---
+++ scripts/run-serial.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIMS=50
+FUNCTION=rastrigin.Rastrigin
+TOPOLOGY=Ring
+PARTICLES=50
+NEIGHBORS=5
+ITERATIONS=5000
+OUTFREQ=100
+
+shortfunc="${FUNCTION%.*}"
+datadir="$HOME/clone/psodata/data_${shortfunc}_${DIMS}/${TOPOLOGY}_${PARTICLES}_${NEIGHBORS}"
+template="iters_${ITERATIONS}_freq_${OUTFREQ}"
+
+mkdir -p "$datadir"
+outfile="$(mktemp --tmpdir=$datadir $template.XXX)"
+
+python standardpso.py -i "$ITERATIONS" --out-freq=$OUTFREQ -b 20 -f $FUNCTION -d $DIMS -t $TOPOLOGY -n $PARTICLES |tee "$outfile"
