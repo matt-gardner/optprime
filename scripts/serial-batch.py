a5c7c82ebbce4dadf641cc33681dd7a0f8ac5e51
Andrew McNabb <amcnabb@mcnabbs.org>
2010-04-01 15:19:00 -0600

    scales the outfreq based on the swarm size and works with complete topology

--- scripts/serial-batch.py
+++ scripts/serial-batch.py
@@ -10,13 +10,14 @@ TOPOLOGY = 'Ring'
 NEIGHBORS_PROPORTION = 0.05
 MIN_ITERS = 5000
 MIN_EVALS = 25000000
-OUTFREQ = 20
+OUTFREQ_EVALS = 100
 
 import math
 import os
 import subprocess
 import tempfile
 
+NEIGHBORLESS_TOPOLOGIES = ('Complete',)
 shortfunc = FUNCTION.split('.')[-1]
 
 
@@ -24,9 +25,14 @@ for particles in PARTICLES:
     iters = max(MIN_ITERS, int(math.ceil(MIN_EVALS / particles)))
     neighbors = int(math.ceil(NEIGHBORS_PROPORTION * particles))
     #neighbors = int(math.ceil(math.log(particles)))
-    datadir = os.path.expanduser('~/clone/psodata/data_%s_%s/%s_%s_%s'
-            % (shortfunc, DIMS, TOPOLOGY, particles, neighbors))
-    template = 'iters_%s_freq_%s' % (iters, OUTFREQ)
+    outfreq = int(math.ceil(OUTFREQ_EVALS / particles))
+    if TOPOLOGY in NEIGHBORLESS_TOPOLOGIES:
+        datadir = os.path.expanduser('~/clone/psodata/data_%s_%s/%s_%s'
+                % (shortfunc, DIMS, TOPOLOGY, particles))
+    else:
+        datadir = os.path.expanduser('~/clone/psodata/data_%s_%s/%s_%s_%s'
+                % (shortfunc, DIMS, TOPOLOGY, particles, neighbors))
+    template = 'iters_%s_freq_%s' % (iters, outfreq)
     try:
         os.makedirs(datadir)
     except OSError:
@@ -37,9 +43,15 @@ for particles in PARTICLES:
                 % (datadir, template))
         proc.stdin.write('cd ~/clone/amlpso\n')
         proc.stdin.write('pwd\n')
-        proc.stdin.write('python standardpso.py -i %s --out-freq=%s'
-            ' -f %s -d %s -t %s -n %s --top-neighbors %s >"$FILENAME"\n'
-            % (iters, OUTFREQ, FUNCTION, DIMS, TOPOLOGY, particles, neighbors))
+        if TOPOLOGY in NEIGHBORLESS_TOPOLOGIES:
+            proc.stdin.write('python standardpso.py -i %s --out-freq=%s'
+                ' -f %s -d %s -t %s -n %s >"$FILENAME"\n'
+                % (iters, outfreq, FUNCTION, DIMS, TOPOLOGY, particles))
+        else:
+            proc.stdin.write('python standardpso.py -i %s --out-freq=%s'
+                ' -f %s -d %s -t %s -n %s --top-neighbors %s >"$FILENAME"\n'
+                % (iters, outfreq, FUNCTION, DIMS, TOPOLOGY, particles,
+                    neighbors))
         proc.stdin.close()
         proc.wait()
 
