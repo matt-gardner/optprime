096e8e3bf190645fbc978a61689c89fea519d894
Andrew McNabb <amcnabb@mcnabbs.org>
2010-04-14 17:08:47 -0600

    added a description of Schwefel

--- functions/schwefel.py
+++ functions/schwefel.py
@@ -3,7 +3,18 @@ from itertools import izip
 import _general
 from math import sqrt, sin
 
+
 class Schwefel(_general._Base):
+    """The Schwefel benchmark function.
+
+    There are other Schwefel benchmark functions; this one is often referred
+    to as "the" Schwefel function.
+
+    Note that this is a constrained optimization problem.  Going outside
+    of the range [-512.03,511.97] is "cheating", so this function doesn't
+    work very well with PSO.
+    """
+
     def setup(self):
         super(Schwefel,self).setup()
         self._set_constraints(((-500,500),) * self.dims)
