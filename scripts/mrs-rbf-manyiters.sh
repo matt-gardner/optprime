0ad7ad396b7438fbad8fbee10c861975ede6702f
Matt Gardner <mjg82@byu.edu>
2011-03-03 08:52:49 -0700

    Updated mrs-rbf-manyiters script

--- scripts/mrs-rbf-manyiters.sh
+++ scripts/mrs-rbf-manyiters.sh
@@ -2,7 +2,8 @@ JOBNAME="rbf-manyiters"
 # rbf dims: num_bases * (1 + 2 * num_input_dimensions)
 $HOME/clone/mrs/examples/potato.py -n "$JOBNAME" -h /admin/potatoes/all6 \
     $HOME/clone/amlpso/specex.py \
-    -f rbf.RBF -d 30 --func-npoints=10000 \
+    -f rbf.RBF -d 30 \
+    --func-npoints=50000 --func-data-noise=2 --func-evenly-spaced \
     -t Rand --top-num=18 --top-neighbors=2 \
     -s PickBestChild -p ManyItersSevenEvals \
-    -i 3000 -b 3 -o Everything
+    -i 1000 -b 3 -o Everything
