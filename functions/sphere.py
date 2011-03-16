e952e84f61858d024337a4dc31f02c0c94c5de77
Matt Gardner <mjg82@byu.edu>
2011-03-16 10:40:07 -0600

    Add SleepSphere function for testing parallelization benefits

--- functions/sphere.py
+++ functions/sphere.py
@@ -1,6 +1,7 @@
 from __future__ import division
 from itertools import izip
 import _general
+from mrs.param import Param
 
 class Sphere(_general._Base):
     def setup(self):
@@ -9,3 +10,15 @@ class Sphere(_general._Base):
 
     def __call__(self, vec):
         return sum([(x-c)**2 for x,c in izip(vec,self.abscenter)])
+
+
+class SleepSphere(Sphere):
+    _params = dict(
+            sleep_time=Param(type='float', default=0.0),
+            )
+
+    def __call__(self, vec):
+        from time import sleep
+        val = super(SleepSphere, self).__call__(vec)
+        sleep(self.sleep_time)
+        return val
