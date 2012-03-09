c9c3675d71afe30985b5d9449fcb0a1ab29354f2
Andrew McNabb <amcnabb@mcnabbs.org>
2012-03-09 11:17:31 -0700

    Modernize the run-subswarms script

--- scripts/run-subswarms.sh
+++ scripts/run-subswarms.sh
@@ -1,4 +1,7 @@
-JOBNAME="ring-subring5"
-SCRATCH_DIR="/prodigy/amcnabb"
-../mrs/examples/potato.py -n "$JOBNAME" -s "$SCRATCH_DIR" -h /admin/potatoes/russet4 -h /admin/potatoes/sweet4 -h /admin/potatoes/yukon4 \
-    ./subswarmpso.py -f rastrigin.Rastrigin -d 50 -t Ring -l Ring --link-num 100 --top-num 1000 --link-neighbors 5 --top-neighbors 25 -s 100 -i 3000 -b 20
+JOBNAME="ring-subring6"
+../mrs/examples/clusterrun.py -n "$JOBNAME" \
+    -h /admin/potatoes/all4 \
+    ./subswarmpso.py -f rastrigin.Rastrigin -d 50 \
+    -t Ring -l Ring --link-num 100 \
+    --top-num 1000 \
+    --link-neighbors 5 --top-neighbors 25 -s 100 -i 3000
