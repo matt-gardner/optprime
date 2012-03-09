765b6075218f9b30fa743b6fab562895861aa413
Andrew McNabb <amcnabb@mcnabbs.org>
2012-03-09 15:03:53 -0700

    Switch to using pypy for subswarms

--- scripts/run-subswarms.sh
+++ scripts/run-subswarms.sh
@@ -1,5 +1,6 @@
 JOBNAME="ring-subring6"
 ../mrs/examples/clusterrun.py -n "$JOBNAME" \
+    --interpreter='PYTHONPATH="$PYTHONPATH:/usr/lib/python2.7/site-packages" pypy' \
     -h /admin/potatoes/all6 \
     ./subswarmpso.py -f rastrigin.Rastrigin -d 50 \
     -t Ring -l Ring --link-num 100 \
