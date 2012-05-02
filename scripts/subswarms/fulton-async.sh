6d82dd8128fd835776ba2ebb2d340f8629477cae
Andrew McNabb <amcnabb@mcnabbs.org>
2012-05-01 18:18:52 -0600

    Fix bad 'interpreter' in fulton-async.sh

--- scripts/subswarms/fulton-async.sh
+++ scripts/subswarms/fulton-async.sh
@@ -1,16 +1,19 @@
-PROCS=512
+PROCS=128
 SUBITERS=100
 # Set to 100*$SUBITERS:
 ITERS=10000
 JOBNAME="a$PROCS-$SUBITERS"
-NODESPEC="nehalem"
+NODESPEC="westmere"
 $HOME/c/mrs/examples/fulton.py -N "$JOBNAME" \
     -n $PROCS -s $PROCS -o "$HOME/out/$JOBNAME" -t 0.5 -m 1 \
     --nodespec "$NODESPEC" \
+    --interpreter /usr/bin/python2.6 \
     $HOME/c/amlpso/subswarmpso.py \
     -f rosenbrock.Rosenbrock -d 250 \
     -l Ring --link-num $PROCS --link-neighbors 1 \
     -t Ring --top-num 5 --top-neighbors 1 \
     -s $SUBITERS -i $ITERS \
     -o TimedBasic --out-freq=0 \
-    --mrs-timing-interval=5
+    --mrs-timing-interval=5 \
+    --async
+
