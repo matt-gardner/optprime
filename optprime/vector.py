74b3394f7e8b28abc4baaec6a26d554ec8e12e90
Andrew McNabb <amcnabb@mcnabbs.org>
2013-02-08 17:32:42 -0700

    Remove dead code in vector

--- optprime/vector.py
+++ optprime/vector.py
@@ -49,37 +49,8 @@ class Vector(tuple):
     def __neg__(self):
         return Vector(-x for x in self)
 
-    def cross3d(self, other):
-        """Cross this vector with the other vector."""
-        assert len(self) == len(other) == 3
-        return Vector((
-                self[1]*other[2]-other[1]*self[2],
-                other[0]*self[2]-self[0]*other[2],
-                self[0]*other[1]-other[0]*self[1],
-            ))
-
-    def distance_to(self, other):
-        return math.sqrt(sum((o-x)**2 for x,o in zip(self, other)))
-
-    def lnorm(self, l):
-        s = 0
-        for v in self:
-            s += v**l
-        return s**(1/l)
-
     def __abs__(self):
         return math.sqrt(sum(x*x for x in self))
-
-    def normalized(self, mag=None):
-        if mag is None:
-            mag = abs(self)
-        if mag > 0:
-            return Vector(x/mag for x in self)
-        else:
-            return self
-
-    def dot(self, other):
-        return sum(x * y for x, y in zip(self,other))
 
 
 def make_binary_op(opname):
