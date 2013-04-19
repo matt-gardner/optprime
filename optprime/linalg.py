286413ce43904b6252f25c0077ca5f2faa5ad26d
Andrew McNabb <amcnabb@mcnabbs.org>
2013-04-19 17:44:46 -0600

    Add a bunch of experimental Bingham-related code (some of which is broken)

--- optprime/linalg.py
+++ optprime/linalg.py
@@ -6,6 +6,8 @@ For numpy arrays vs. matrices, see:
 
 from __future__ import division, print_function
 
+import itertools
+import math
 import random
 
 try:
@@ -124,5 +126,132 @@ def orthogonalize(x, A):
     v /= np.sqrt(np.dot(v, v))
     return v
 
+def kume_walker_phi(p, M, N):
+    """Return the truncation error estimate from Kume-Walker.
 
+    M = max{a_i; i=1,...,p, |b_i|; i=0,...,p}
+
+    Phi is a bound on the sum of all of the w(l) where \sum l_i >= N.
+    """
+    log_tau = p * math.lgamma(0.5) - math.lgamma(p / 2 + 0.5)
+    log_phi = (log_tau
+            + N * (math.log(M) + math.log(1 + (p + 1) ** 0.5))
+            - math.lgamma(N + 1)
+            + M * (1 + (p + 1) ** 0.5))
+    return math.exp(log_phi)
+
+def kume_walker_phi2(p, M, N):
+    """Return the truncation error estimate from Kume-Walker (modified).
+
+    M = max{a_i; i=1,...,p, |b_i|; i=0,...,p}
+
+    Phi is a bound on the sum of all of the w(l) where \sum l_i >= N.
+    """
+    log_tau = p * math.lgamma(0.5) - math.lgamma(p / 2 + 0.5)
+    log_phi = (log_tau
+            + N * (math.log(M) + 0.5 * math.log(p + 1))
+            - math.lgamma(N + 1)
+            + M * (p + 1) ** 0.5)
+    return math.exp(log_phi)
+
+def kume_walker_w(l, a):
+    log_w = math.lgamma(0.5)
+    log_w -= math.lgamma(0.5 + sum((l_i + 0.5) for l_i in l))
+    for l_i, a_i in zip(l, a):
+        log_w += l_i * math.log(a_i) + math.lgamma(l_i + 0.5)
+        log_w -= math.lgamma(l_i + 1)
+    return math.exp(log_w)
+
+def kume_walker_norm(a, N):
+    """Sum up all of the w(l) terms with \sum l_i < N."""
+    indices = range(len(a))
+    total = 0
+    for cap in range(N):
+        print('CAP:', cap)
+        #print(list(itertools.combinations_with_replacement(indices, cap)))
+        for combos in itertools.combinations_with_replacement(indices, cap):
+            l = [0] * len(a)
+            for i in combos:
+                l[i] += 1
+            total += kume_walker_w(l, a)
+            print(l)
+        print('total:', total)
+    return total
+
+def kume_walker_norm2(a_i, p, N):
+    """Sum up all of the w(l) terms with \sum l_i < N.
+
+    This variant assumes that a_i = a_1 for all i.  Note that p is the number
+    of dimensions.
+    """
+    w_i_terms = []
+    for l_i in range(N):
+        # The log of the term of w(l) corresponding to a single i (within the
+        # product).
+        term = (l_i * math.log(a_i) + math.lgamma(l_i + 0.5)
+                - math.lgamma(l_i + 1))
+        w_i_terms.append(term)
+
+    total = 0
+    for cap in range(N):
+        print('CAP:', cap)
+        # The log of the base term (outside the product) in w(l)
+        base_term = math.lgamma(0.5) - math.lgamma(0.5 * (p + 1) + cap)
+
+        for part in partitions(cap):
+            if len(part) > p:
+                continue
+            print(part)
+
+            log_w = base_term
+            # Note that partitions(0) returns [0] (for better or worse).
+            if part[0] == 0:
+                part_size = 0
+            else:
+                for i, l_i in enumerate(part):
+                    log_w += w_i_terms[l_i]
+                part_size = i + 1
+            # Add in all of the 0 terms.
+            log_w += (p - part_size) * w_i_terms[0]
+
+            # The binomial coefficient (choose function) indicates the
+            # number of identical terms (i.e., the number of l vectors
+            # that can be formed by rearranging this partition).
+            log_count = (math.lgamma(p + 1) - math.lgamma(part_size + 1)
+                    - math.lgamma(p - part_size + 1))
+            total += math.exp(log_w + log_count)
+        print('total:', total)
+
+    return total
+
+def partitions(n):
+    """Generate all integer partitions of n (accelAsc)
+
+    Kelleher and O'Sullivan. Generating All Partitions: A Comparison of Two
+    Encodings. 2009.
+    See: http://homepages.ed.ac.uk/jkellehe/partitions.php
+
+    See http://arxiv.org/abs/0909.2331
+    """
+    a = [0] * (n + 1)
+    k = 1
+    a[0] = 0
+    y = n - 1
+    while k != 0:
+        x = a[k - 1] + 1
+        k -= 1
+        while 2*x <= y:
+            a[k] = x
+            y -= x
+            k += 1
+        l = k + 1
+        while x <= y:
+            a[k] = x
+            a[l] = y
+            yield a[:k + 2]
+            x += 1
+            y -= 1
+        a[k] = x + y
+        y = x + y - 1
+        yield a[:k + 1]
 # vim: et sw=4 sts=4
