226861a0ed38359084e608583abc23fd6a60a750
Andrew McNabb <amcnabb@mcnabbs.org>
2013-07-29 16:13:24 -0600

    Separate tests for BinghamSampler and ComplexBinghamSampler

--- tests/test_linalg.py
+++ tests/test_linalg.py
@@ -130,46 +130,71 @@ def test_bingham_sampler_init_neg():
     bs = BinghamSampler(A)
     assert list(bs._lambdas) == [8, 6]
 
-def test_bingham_pick_sampler1():
+def test_complex_bingham_sampler_init():
+    # Note that this test array is symmetric.
+    A = np.array([[5, 0, 0], [0, 3, 0], [0, 0, 1]])
+
+    bs = ComplexBinghamSampler(A)
+    assert list(bs._lambdas) == [4, 2]
+
+def test_complex_bingham_sampler_dual():
+    # Note that this test array is symmetric.
+    A = np.array([[5, 0, 0], [0, 3, 0], [0, 0, 1]])
+
+    bs = ComplexBinghamSampler(A)
+    bs_dual1 = ComplexBinghamSampler(-A)
+    bs_dual2 = bs.dual()
+
+    assert np.allclose(bs_dual1._lambdas, bs_dual2._lambdas)
+    assert np.allclose(bs_dual1._eigvecs, bs_dual2._eigvecs)
+
+def test_complex_bingham_sampler_init_neg():
+    # Note that this test array is symmetric.
+    A = np.array([[-3, 0, 0], [0, 5, 0], [0, 0, -1]])
+
+    bs = ComplexBinghamSampler(A)
+    assert list(bs._lambdas) == [8, 6]
+
+def test_complex_bingham_pick_sampler1():
     """Values from Table 2 (Kent, Constable, and Er, 2004)."""
     lambdas = [0.01, 0.01]
-    bs = BinghamSampler(lambdas=lambdas)
+    bs = ComplexBinghamSampler(lambdas=lambdas)
     assert bs._pick_sampler() == bs.sample_m2
 
-def test_bingham_pick_sampler2():
+def test_complex_bingham_pick_sampler2():
     """Values from Table 2 (Kent, Constable, and Er, 2004)."""
     lambdas = [0.1, 0.01]
-    bs = BinghamSampler(lambdas=lambdas)
+    bs = ComplexBinghamSampler(lambdas=lambdas)
     assert bs._pick_sampler() == bs.sample_m2
 
-def test_bingham_pick_sampler3():
+def test_complex_bingham_pick_sampler3():
     """Values from Table 2 (Kent, Constable, and Er, 2004)."""
     lambdas = [0.5, 0.01]
-    bs = BinghamSampler(lambdas=lambdas)
+    bs = ComplexBinghamSampler(lambdas=lambdas)
     assert bs._pick_sampler() == bs.sample_m2
 
-def test_bingham_pick_sampler4():
+def test_complex_bingham_pick_sampler4():
     """Values from Table 2 (Kent, Constable, and Er, 2004)."""
     lambdas = [1.0, 0.01]
-    bs = BinghamSampler(lambdas=lambdas)
+    bs = ComplexBinghamSampler(lambdas=lambdas)
     assert bs._pick_sampler() == bs.sample_m1
 
-def test_bingham_pick_sampler5():
+def test_complex_bingham_pick_sampler5():
     """Values from Table 2 (Kent, Constable, and Er, 2004)."""
     lambdas = [0.1, 0.1]
-    bs = BinghamSampler(lambdas=lambdas)
+    bs = ComplexBinghamSampler(lambdas=lambdas)
     assert bs._pick_sampler() == bs.sample_m2
 
-def test_bingham_pick_sampler6():
+def test_complex_bingham_pick_sampler6():
     """Values from Table 2 (Kent, Constable, and Er, 2004)."""
     lambdas = [1.0, 0.1]
-    bs = BinghamSampler(lambdas=lambdas)
+    bs = ComplexBinghamSampler(lambdas=lambdas)
     assert bs._pick_sampler() == bs.sample_m1
 
-def test_bingham_pick_sampler7():
+def test_complex_bingham_pick_sampler7():
     """Values from Table 2 (Kent, Constable, and Er, 2004)."""
     lambdas = [0.5, 0.5]
-    bs = BinghamSampler(lambdas=lambdas)
+    bs = ComplexBinghamSampler(lambdas=lambdas)
     assert bs._pick_sampler() == bs.sample_m1
 
 def test_von_mises_fisher_norm():
